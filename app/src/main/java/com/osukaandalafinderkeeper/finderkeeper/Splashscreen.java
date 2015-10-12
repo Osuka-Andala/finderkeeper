cd9511be8e67172a0b08c33aa6cda4e5b1056cd3
Osuka-Andala <teresa.andala@gmail.com>
2015-10-12 18:09:36 +0300

    Added run time to splashscreen.java

--- app/src/main/java/com/osukaandalafinderkeeper/finderkeeper/Splashscreen.java
+++ app/src/main/java/com/osukaandalafinderkeeper/finderkeeper/Splashscreen.java
@@ -1,5 +1,6 @@
 package com.osukaandalafinderkeeper.finderkeeper;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -11,6 +12,22 @@ public class Splashscreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splashscreen);
+
+        Thread splash_screen = new Thread(){
+            @Override
+        public void  run(){
+                try{
+                    sleep(2000);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                finally {
+                    startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                    finish();
+                }
+            }
+        };
+        splash_screen.start();
     }
 
     @Override
