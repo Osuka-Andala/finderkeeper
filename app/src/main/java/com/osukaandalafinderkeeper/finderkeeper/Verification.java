7ec6ffda5b5b1730b6d41290dfe7bec243ae450a
Osuka-Andala <teresa.andala@gmail.com>
2015-10-13 23:26:08 +0300

    Added phone number details

--- app/src/main/java/com/osukaandalafinderkeeper/finderkeeper/Verification.java
+++ app/src/main/java/com/osukaandalafinderkeeper/finderkeeper/Verification.java
@@ -2,8 +2,15 @@ package com.osukaandalafinderkeeper.finderkeeper;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.digits.sdk.android.AuthCallback;
+import com.digits.sdk.android.DigitsAuthButton;
+import com.digits.sdk.android.DigitsException;
+import com.digits.sdk.android.DigitsSession;
 
 public class Verification extends AppCompatActivity {
 
@@ -11,6 +18,22 @@ public class Verification extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_verification);
+
+        DigitsAuthButton digitsButton = (DigitsAuthButton) findViewById(R.id.auth_button);
+        digitsButton.setCallback(new AuthCallback() {
+            @Override
+            public void success(DigitsSession session, String phoneNumber) {
+                // TODO: associate the session userID with your user model
+                Toast.makeText(getApplicationContext(), "Authentication successful for "
+                        + phoneNumber, Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void failure(DigitsException exception) {
+                Log.d("Digits", "Sign in with Digits failure", exception);
+            }
+        });
+
     }
 
     @Override
